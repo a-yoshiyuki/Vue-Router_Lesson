c0f5a5317bb9d44518709e7f72861e56c33aa7e6
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-07-21 13:14:06 +0000

    [Add]Lesson-188

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <nav style="width: 700px; margin: auto; padding-top: 50px;">
+      <router-link to="/" active-class="link--active" exact class="link">Home</router-link>
+      <router-link to="/users" active-class="link--active" exact class="link">Users</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .link {
+    margin-right: 10px;
+  }
+
+  .link--active {
+    font-size: 20px;
+  }
+</style>
