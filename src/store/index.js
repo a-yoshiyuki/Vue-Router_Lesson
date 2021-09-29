c8a4570a19f044ada16c04584fa1cfe6a8e96093
a-yoshiyuki <a.yoshiyuki3285@gmail.com>
2021-09-29 01:49:06 +0000

    Lesson_221

--- src/store/index.js
+++ src/store/index.js
@@ -1,39 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import count from './modules/count';
 
 Vue.use(Vuex);
 
 export default
 new Vuex.Store({
   state: {
-    count: 2,
     messege: ""
   },
   getters: {
-    doubleCount: state => state.count * 2,
-    tripleCount: state => state.count * 3,
     messege: state => state.messege
   },
   mutations: {
-    increment(state, number) {
-      state.count += number;
-    },
-    decrement(state, number) {
-      state.count -= number;
-    },
     updateMessege(state, newMessege) {
       state.messege = newMessege;
     }
   },
   actions: {
-    increment({ commit }, number) {
-      commit('increment', number);
-    },
-    decrement({ commit }, number) {
-      commit('decrement', number);
-    },
     updateMessege({commit}, newMessege) {
       commit("updateMessege", newMessege);
     }
+  },
+  modules: {
+    count
   }
 });
